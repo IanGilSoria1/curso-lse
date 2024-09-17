0ea6506c3533d4580e3ebf3653ab614bee47a838
IanGilSoria1 <ianlucasgilsoria@gmail.com>
2024-09-17 12:50:54 -0300

    chore: archivos por construccion

---
+++ workspace_lpc845/01_animation/source/01_animation.c
@@ -0,0 +1,44 @@
+#include "board.h"
+#include "LPC845.h"
+
+// Etiqueta para el pulsador USER
+#define USER_BTN	4
+// Etiqueta para el LED azul
+#define LED_BLUE	2
+
+/*
+ * @brief   Application entry point.
+*/
+int main(void) {
+
+	// Inicializacion
+    BOARD_InitBootClocks();
+
+    // Estructura de configuracion para salida
+    gpio_pin_config_t out_config = { kGPIO_DigitalOutput, 1 };
+    // Estructura de configuracion para entrada
+    gpio_pin_config_t in_config = { kGPIO_DigitalInput };
+    // Habilito el puerto 0
+    GPIO_PortInit(GPIO, 0);
+    //Habilito segmento "a"
+    GPIO_PortInit (GPIO, 10);
+    // Configuro el pin 4 como entrada
+    GPIO_PinInit(GPIO, 0, USER_BTN, &in_config);
+    // Habilito el clock del GPIO 1
+    GPIO_PortInit(GPIO, 1);
+
+    while(1) {
+
+    	// Verifico el estado del pulsador (tiene pull-up)
+    	if(!GPIO_PinRead(GPIO, 0, USER_BTN)) {
+    		// Prendo el segmento (anodo comun)
+    		GPIO_PinWrite(GPIO, 10, LED_BLUE, 0);
+    	}
+    	else {
+    		// Apago el segmento de manera secuenciada
+    		GPIO_PinWrite(GPIO, 10, LED_BLUE, 1);
+    	}
+    }
+
+    return 0;
+}
