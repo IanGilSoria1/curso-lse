b5d7d67b4a4f56eb7dddcc164782305d14e1fd81
IanGilSoria1 <ianlucasgilsoria@gmail.com>
2024-09-25 07:41:56 -0300

    feat: inicializacion del codigo de animacion.c

--- workspace_lpc845/01_animation/source/01_animation.c
+++ workspace_lpc845/01_animation/source/01_animation.c
@@ -3,8 +3,16 @@
 
 // Etiqueta para el pulsador USER
 #define USER_BTN	4
-// Etiqueta para el LED azul
-#define LED_BLUE	2
+//Etiqueta Digito A de luz
+#define DIGITO 8
+//Etiqueta segmento 1 de luz
+#define SEGMENTA 9
+#define SEGMENTE 10
+#define SEGMENTI 11
+#define SEGMENTO 12
+#define SEGMENTU 13
+#define SEGMENTAA 14
+#define SEGMENTEE 15
 
 /*
  * @brief   Application entry point.
@@ -13,30 +21,37 @@ int main(void) {
 
 	// Inicializacion
     BOARD_InitBootClocks();
+    GPIO_PortInit(GPIO, 0);
 
     // Estructura de configuracion para salida
     gpio_pin_config_t out_config = { kGPIO_DigitalOutput, 1 };
     // Estructura de configuracion para entrada
     gpio_pin_config_t in_config = { kGPIO_DigitalInput };
     // Habilito el puerto 0
-    GPIO_PortInit(GPIO, 0);
+
     //Habilito segmento "a"
-    GPIO_PortInit (GPIO, 10);
     // Configuro el pin 4 como entrada
-    GPIO_PinInit(GPIO, 0, USER_BTN, &in_config);
-    // Habilito el clock del GPIO 1
-    GPIO_PortInit(GPIO, 1);
+    GPIO_PinInit(GPIO, 0, USER_BTN , &in_config);
+    //Configuro el pin 8 como saida
+    GPIO_PinInit(GPIO, 0, DIGITO , &out_config);
+    //Configuro el pin 8 como saida
+    GPIO_PinInit(GPIO, 0, SEGMENTA , &out_config);
 
     while(1) {
 
     	// Verifico el estado del pulsador (tiene pull-up)
     	if(!GPIO_PinRead(GPIO, 0, USER_BTN)) {
     		// Prendo el segmento (anodo comun)
-    		GPIO_PinWrite(GPIO, 10, LED_BLUE, 0);
+    		GPIO_PinWrite(GPIO, 0, DIGITO, 0);
+    		GPIO_PinWrite(GPIO, 0, SEGMENTA, 0);
+
+
     	}
     	else {
     		// Apago el segmento de manera secuenciada
-    		GPIO_PinWrite(GPIO, 10, LED_BLUE, 1);
+    		GPIO_PinWrite(GPIO, 0, DIGITO, 1);
+    		GPIO_PinWrite(GPIO, 0, SEGMENTA, 1);
+
     	}
     }
 
